8b831b352f4cac826580d11a6eab6a5139fc079e
VKon <vincent373kon@gmail.com>
2024-08-03 22:39:34 +0700

    create function to format currency fixed by 2 and apply it

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,7 @@
 import { cart, addToCart } from "../data/cart.js";
 import { products } from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
+
 let productsHTML = "";
 products.forEach(function (product, index) {
   productsHTML += `
@@ -25,8 +27,8 @@ products.forEach(function (product, index) {
             }</div>
           </div>
 
-          <div class="product-price">$${(product.priceCents / 100).toFixed(
-            2
+          <div class="product-price">$${formatCurrency(
+            product.priceCents
           )}</div>
 
           <div class="product-quantity-container">
